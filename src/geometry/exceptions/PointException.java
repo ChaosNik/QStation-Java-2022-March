f4b084eef5aaa3c7844c0c4c98bde4addb1c4123
DESKTOP-7JLN7TV\Korisnik <biljana19848314@gmail.com>
2022-05-20 13:28:54 +0200

    Implementacija PointException

---
+++ src/geometry/exceptions/PointException.java
@@ -0,0 +1,16 @@
+package geometry.exceptions;
+
+public class PointException extends GeometricException
+{
+	private static final long serialVersionUID = 1L;
+	public PointException()
+	{
+		super("");
+	}
+	public PointException(String message)
+	{
+		super(message);
+	}
+
+
+}
